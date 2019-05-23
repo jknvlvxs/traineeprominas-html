3452877967f7a52520c7edb9fe4ed2fb68a56b39
jose julio <julio619alves@gmail.com>
2019-05-23 14:02:47 -0300

    Desafio Finalizado - Semana 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#btnIndex').click(function(){
+        var e = document.getElementById("pgSelect");
+        var itemSelecionado = e.options[e.selectedIndex].value;
+    if(itemSelecionado === 1){
+        window.location.assign("http://localhost:63342/Desafio_Semana1/autorizado.html");
+    }else if(itemSelecionado === 2){
+        window.location.assign("http://localhost:63342/Desafio_Semana1/errooperadora.html");
+    }else{
+        window.location.assign("http://localhost:63342/Desafio_Semana1/errodesconhecido.html");
+    }
+
+    })
+});
