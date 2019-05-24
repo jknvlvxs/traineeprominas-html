2556234f5ab78cf6b3428dfc0f9fb0f5aeadcbb4
jose julio <julio619alves@gmail.com>
2019-05-24 10:44:47 -0300

    Atualiza links - Desafio 1

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(function(){
         var e = document.getElementById("pgSelect");
         var itemSelecionado = e.options[e.selectedIndex].value;
     if(itemSelecionado == 1){
-        window.location.assign("http://localhost:63342/Desafio_Semana1/autorizado.html");
+        window.location.assign("autorizado.html");
     }else if(itemSelecionado == 2){
-        window.location.assign("http://localhost:63342/Desafio_Semana1/errooperadora.html");
+        window.location.assign("errooperadora.html");
     }else{
-        window.location.assign("http://localhost:63342/Desafio_Semana1/errodesconhecido.html");
+        window.location.assign("errodesconhecido.html");
     }
 
     })
