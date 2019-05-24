69527f982d0b48641368c0f117c38db511c51680
jose julio <julio619alves@gmail.com>
2019-05-24 11:34:19 -0300

    Removendo .html do link

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ $(function(){
         var e = document.getElementById("pgSelect");
         var itemSelecionado = e.options[e.selectedIndex].value;
     if(itemSelecionado == 1){
-        window.location.assign("autorizado.html");
+        window.location.assign("autorizado");
     }else if(itemSelecionado == 2){
-        window.location.assign("errooperadora.html");
+        window.location.assign("errooperadora");
     }else{
         window.location.assign("errodesconhecido");
     }
