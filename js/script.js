fe27457698ccd321949ff81934f368ae29b29c36
jose julio <julio619alves@gmail.com>
2019-05-24 10:49:14 -0300

    Adiciona / no link

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(function(){
         var e = document.getElementById("pgSelect");
         var itemSelecionado = e.options[e.selectedIndex].value;
     if(itemSelecionado == 1){
-        window.location.assign("autorizado.html");
+        window.location.assign("/autorizado.html");
     }else if(itemSelecionado == 2){
-        window.location.assign("errooperadora.html");
+        window.location.assign("/errooperadora.html");
     }else{
-        window.location.assign("errodesconhecido.html");
+        window.location.assign("/errodesconhecido.html");
     }
 
     })
